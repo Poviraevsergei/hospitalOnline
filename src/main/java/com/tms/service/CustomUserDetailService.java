68d991d2fc64192ea4078ed650388c65ccd0350d
Poviraevsergei <pacmanmozg1441>
2023-03-15 21:38:43 +0300

    Added CustomUserDetailService for Margarita

---
+++ src/main/java/com/tms/service/CustomUserDetailService.java
@@ -0,0 +1,33 @@
+package com.tms.service;
+
+import com.tms.domain.User;
+import com.tms.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomUserDetailService implements UserDetailsService {
+
+
+    UserRepository userRepository;
+
+    @Autowired
+    public CustomUserDetailService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
+        User user = userRepository.findUserByLogin(s);
+        if (user == null) {
+            throw new UsernameNotFoundException(s);
+        }
+        return org.springframework.security.core.userdetails.User
+                .withUsername(user.getLogin())
+                .password(user.getPassword())
+                .roles(userRepository.getRole(user.getId())).build();
+    }
+}
